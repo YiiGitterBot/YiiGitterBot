b726554bc7dd2541a900179040c41695fe6d3e64
Евгений Андреевич < @home-pc>
2015-10-20 20:20:59 +0300

    ---

--- src/yiigitterbot/YiiGitterBot.java
+++ src/yiigitterbot/YiiGitterBot.java
@@ -5,9 +5,9 @@ public class YiiGitterBot {
     /**
      * Settings Gitter REST & Streaming API
      */
-     static final String gitterToken = "873b11943ff0bfc4e4c5d6989f96da616121d246";
-    static final String gitterRoomId = "56266df816b6c7089cb7a9a7"; // 5624245a16b6c7089cb770cc
-    static final String idGitterBot = "562567a916b6c7089cb79037";
+     static final String gitterToken = "873b11943ff0bfc4e4c5d6989f96da616121d246"; //https://developer.gitter.im/apps
+    static final String gitterRoomId = "56266df816b6c7089cb7a9a7";
+    static final String idGitterBot = "562567a916b6c7089cb79037"; // curl -i -H "Accept: application/json" -H "Authorization: Bearer 33caf7e2c9953a0dcbd7b5745bd4569ff17cf566" "https://api.gitter.im/v1/user"
     
     static final String gitterRestUrl = "https://api.gitter.im/v1/";
     static final String gitterStreamingUrl = "https://stream.gitter.im/v1/rooms/";
