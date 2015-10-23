c7acc8ac9a37b3c6e07bdb97a97018eec4d83519
Alex Bond <alex.bondarevskiy@gmail.com>
2015-10-22 19:10:50 -0700

    Small code refraction

--- src/main/java/org/YiiCommunity/GitterBot/achievements/FirstThankYou.java
+++ src/main/java/org/YiiCommunity/GitterBot/achievements/FirstThankYou.java
@@ -1,21 +1,21 @@
 package org.YiiCommunity.GitterBot.achievements;
 
-import org.YiiCommunity.GitterBot.api.Achievement;
+import org.YiiCommunity.GitterBot.models.postgres.Achievement;
 import org.YiiCommunity.GitterBot.containers.Gitter;
 import org.YiiCommunity.GitterBot.models.postgres.User;
 
 /**
  * Created by Alex on 10/20/15.
  */
-public class FirstThankYou implements Achievement {
+public class FirstThankYou implements org.YiiCommunity.GitterBot.api.Achievement {
     private String name = "firstThankYou";
 
     @Override
     public void onUserChange(User user) {
         if (!user.hasAchievement(name))
             try {
-                user.addAchievement(org.YiiCommunity.GitterBot.models.postgres.Achievement.getAchievement(name));
-                org.YiiCommunity.GitterBot.models.postgres.Achievement obj = org.YiiCommunity.GitterBot.models.postgres.Achievement.getAchievement(name);
+                user.addAchievement(Achievement.getAchievement(name));
+                Achievement obj = Achievement.getAchievement(name);
                 Gitter.sendMessage(obj.getChatAnnounce().replace("{username}", user.getUsername()));
             } catch (Exception e) {
                 e.printStackTrace();
