9a48bd819ea70651c92024dafdb5f86e430e4fdc
Alex Bond <alex.bondarevskiy@gmail.com>
2015-10-23 04:29:48 -0700

    Достужения в команде "карма"

--- src/main/java/org/YiiCommunity/GitterBot/commands/Carma.java
+++ src/main/java/org/YiiCommunity/GitterBot/commands/Carma.java
@@ -4,6 +4,7 @@ import org.YiiCommunity.GitterBot.api.Command;
 import org.YiiCommunity.GitterBot.containers.Gitter;
 import org.YiiCommunity.GitterBot.models.json.Message;
 import org.YiiCommunity.GitterBot.models.postgres.User;
+import org.YiiCommunity.GitterBot.models.postgres.UserAchievements;
 import org.YiiCommunity.GitterBot.utils.L;
 import org.YiiCommunity.GitterBot.utils.yuml.file.FileConfiguration;
 import org.YiiCommunity.GitterBot.utils.yuml.file.YamlConfiguration;
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * Created by Alex on 10/23/15.
@@ -31,7 +33,7 @@ public class Carma implements Command {
             for (String item : commands) {
                 if (message.getText().equalsIgnoreCase(item)) {
                     User user = User.getUser(message.getFromUser().getUsername());
-                    Gitter.sendMessage("@" + user.getUsername() + " твоя карма нынче **" + (user.getCarma() >= 0 ? "+" : "-") + user.getCarma() + "**");
+                    Gitter.sendMessage("@" + user.getUsername() + " твоя карма нынче **" + (user.getCarma() >= 0 ? "+" : "-") + user.getCarma() + "**\n" + getAchievements(user));
                     return;
                 }
             }
@@ -40,11 +42,19 @@ public class Carma implements Command {
 
             while (m.find()) {
                 User receiver = User.getUser(m.group(1));
-                Gitter.sendMessage("Карма @" + receiver.getUsername() + " нынче **" + (receiver.getCarma() >= 0 ? "+" : "-") + receiver.getCarma() + "**");
+                Gitter.sendMessage("Карма @" + receiver.getUsername() + " нынче **" + (receiver.getCarma() >= 0 ? "+" : "-") + receiver.getCarma() + "**\n" + getAchievements(receiver));
                 receiver.updateAchievements();
             }
         } catch (Exception e) {
             L.$(e.getMessage());
         }
+    }
+
+    private String getAchievements(User user) {
+        if (user.getAchievements().isEmpty())
+            return "Пока достижений нет.";
+
+        List<String> list = user.getAchievements().stream().map(item -> item.getAchievement().getTitle()).collect(Collectors.toList());
+        return "Достижения: " + String.join(", ", list);
     }
 }
