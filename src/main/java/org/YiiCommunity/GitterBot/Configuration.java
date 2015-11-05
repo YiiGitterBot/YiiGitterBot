51c4789f90a1ef42659ef23e8d1e2b061f65490c
Alex Bond <alex.bondarevskiy@gmail.com>
2015-11-05 03:21:13 -0800

    User avatar in db

--- src/main/java/org/YiiCommunity/GitterBot/Configuration.java
+++ src/main/java/org/YiiCommunity/GitterBot/Configuration.java
@@ -3,6 +3,7 @@ package org.YiiCommunity.GitterBot;
 import lombok.Getter;
 import org.YiiCommunity.GitterBot.containers.Gitter;
 import org.YiiCommunity.GitterBot.utils.JarUtils;
+import org.YiiCommunity.GitterBot.utils.L;
 import org.YiiCommunity.GitterBot.utils.yuml.file.FileConfiguration;
 import org.YiiCommunity.GitterBot.utils.yuml.file.YamlConfiguration;
 
@@ -14,6 +15,8 @@ public class Configuration {
     private FileConfiguration config;
 
     private String gitterToken;
+
+    private String gitHubToken;
 
     private String botUsername;
 
@@ -27,6 +30,8 @@ public class Configuration {
         }
         config = YamlConfiguration.loadConfiguration(new File("config.yml"));
         gitterToken = config.getString("gitter.token");
+
+        gitHubToken = config.getString("github.token");
     }
 
     public String getBotUsername() {
